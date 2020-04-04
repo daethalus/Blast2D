0d2236cbe4a29ea0cabf0c25272cbcae1089af55
daethalus <daethalus@gmail.com>
2020-04-04 20:49:18 +0100

    new ecs

--- Blast2D/ecs/entity_manager.hpp
+++ Blast2D/ecs/entity_manager.hpp
@@ -1,155 +1,18 @@
 #ifndef ENTITY_MANAGER_HPP
 #define ENTITY_MANAGER_HPP
 
-#include <ecs/component.hpp>
-#include <ecs/entity.hpp>
-#include <ecs/view.hpp>
-#include <ecs/component_data_pool.hpp>
-
+#include <unordered_map>
+#include <memory>
 #include <functional>
 
+
 namespace Blast2D {
+
 	class EntityManager {
-	private:		
-		ComponentRegister componentRegister;
-		EntityPool entityPool;
-
-		mutable std::vector<PoolData> pools;		
-		std::unordered_map<Component, SingletonData> singletons;
-
-
-		template <typename Type>
-		Singleton<Type>& assureSingleton(Component component) {
-			if (singletons.count(component) <= 0) {
-				singletons[component] = { new Singleton<Type>() };
-			}
-			return static_cast<Singleton<Type>&>(*singletons[component].data);
-		}
-
-		template <typename Type>
-		PoolHandler<Type>& assure(Component component) {
-			if (pools.size() <= component) {
-				pools.resize(component + 1);
-			}
-			if (pools[component].pool == nullptr) {
-				pools[component] = { new PoolHandler<Type>() };
-			}
-			return static_cast<PoolHandler<Type>&>(*pools[component].pool);
-		}
-
-		BasicView internalView(std::vector<Component> comps) {
-			if (comps.size() > 1) {
-				BasicView entities;
-				auto first = pools[comps[0]].pool;
-				auto pos = 0;
-
-				for (int x = 1; x < comps.size(); ++x) {
-					if (pools[comps[x]].pool->size() < first->size()) {
-						first = pools[comps[x]].pool;
-						pos = x;
-					}
-				}
-				comps.erase(comps.begin() + pos);
-
-				for (const auto& entity : first->view()) {
-					bool found = true;
-					for (const auto& comp : comps) {
-						if (!pools[comp].pool->entityIn(entity)) {
-							found = false;
-							break;
-						}
-					}
-					if (found) {
-						entities.push_back(entity);
-					}
-				}
-				return entities;
-			} else {
-				return pools[comps[0]].pool->view();
-			}
-		}
-
 	public:
-		template <typename Type>
-		ComponentInfo registerComponent(std::string name) {
-			ComponentInfo info;
-			info.id = typeid(Type).hash_code();
-			info.name = name;
-			componentRegister.registerNewComponent(info);
-			return info;
-		}
-
-		Entity createEntity() {
-			return entityPool.generate();
-		}
-
-		void destroyEntity(Entity entity) {
-			entityPool.release(entity);
-		}
-
-		template <typename Type>
-		Component getComponent() {
-			return componentRegister.get(typeid(Type).hash_code()).index;			
-		}		
-
-		template <typename Type>
-		void assign(Entity entity, Type instance) {
-			auto comp = getComponent<Type>();
-			auto& pool = this->assure<Type>(comp);
-			pool.assign(entity, instance);
-		}
-
-		template <typename Type>
-		void assign(Entity entity, Component comp, Type instance) {			
-			this->assure<Type>(comp).assign(entity, instance);
-		}
-
-		template <typename Type>
-		Type& get(Entity entity) {					
-			return this->assure<Type>(getComponent<Type>()).get(entity);
-		}
-
-		template <typename T>
-		void listComponents(std::vector<Component>& vcomp, T component) {
-			vcomp.push_back(component);
-		}
-
-		template <typename T, typename... Args>
-		void listComponents(std::vector<Component>& vcomp, T component, Args... components) {
-			vcomp.push_back(component);
-			listComponents(vcomp, components...);
-		}
-
-		template<typename... Components>
-		typename std::enable_if<sizeof...(Components) == 0>::type find(std::vector<Component>& componentIds) {
-		}
 
 
-		template<typename Type, typename... Components>
-		void find(std::vector<Component>& componentIds) {
-			componentIds.push_back(getComponent<Type>());
-			find<Components...>(componentIds);
-		}
-
-		template <typename... Components>
-		const BasicView view() {
-			std::vector<Component> componentIds;
-			find<Components...>(componentIds);
-			return internalView(componentIds);
-		}
-
-
-		template <typename... Args>
-		BasicView view(Args... components) {
-			std::vector<Component> comps;
-			listComponents(comps, components...);
-			return internalView(comps);
-		}
-
-		BasicView view(std::vector<Component> comps) {
-			return internalView(comps);
-		}
 	};
+	
 }
-
 #endif
--- Blast2D/main.cpp
+++ Blast2D/main.cpp
@@ -4,114 +4,17 @@
 #include <ecs/entity_manager.hpp>
 #include <functional>
 
+#include "entt.hpp"
+
 INITIALIZE_EASYLOGGINGPP
 
-template<typename>
-struct fnv1a_traits;
 
-template<>
-struct fnv1a_traits<std::uint32_t> {
-	using type = std::uint32_t;
-	static constexpr std::uint32_t offset = 2166136261;
-	static constexpr std::uint32_t prime = 16777619;
-};
-
-template<typename Type>
-struct TestX {
-
-	using traits_type = fnv1a_traits<std::uint32_t>;
-
-	static constexpr std::uint32_t helper(const char* curr) {
-		auto value = traits_type::offset;
-
-		while (*curr != 0) {
-			value = (value ^ static_cast<traits_type::type>(*(curr++)))* traits_type::prime;
-		}
-
-		return value;
-	}
-
-	constexpr static std::uint32_t id(std::string str) {
-		return helper(str.c_str());
-	}
-
-	constexpr static std::uint32_t id() {
-		//return helper(__FUNCSIG__);
-		return helper(typeid(Type).name());
-	}
-
-};
-
-struct Test {
-	int value;
-};
-
-struct Position {
-	int value;
-};
-
-struct Data {
-	std::uint32_t id;
-	int value;
-};
 
 int main() {
-
-	std::vector<Data> pools(2);
-
-	pools[1] = { TestX<Position>::id(), 10 };
-
 	LOG(INFO) << "Starting engine";
 
+	
 	Blast2D::EntityManager entityManager;
-	Blast2D::ComponentRegister reg;
-
-	LOG(INFO) << TestX<Position>::id("asd");
-	LOG(INFO) << TestX<Position>::id();
-	LOG(INFO) << TestX<Test>::id();
-		
-	auto chrono = Blast2D::Chronometer::create();	
-
-	auto hash = std::hash<std::string>();
-
-	for (int x = 0; x <= 380; ++x) {
-		Blast2D::ComponentInfo info = { x, hash(std::to_string(x)),std::to_string(x) };
-		reg.registerNewComponent(info);
-	}
-
-	for (auto x = 0; x <= 999999; ++x) {
-
-
-		static std::size_t index{ pools.size() };
-		if (const auto length = pools.size(); !(index < length) || pools[index].id != TestX<Position>::id()) {
-			for (index = {}; index < length && pools[index].id != TestX<Position>::id(); ++index);
-		}
-
-		if (pools[index].value != 10) {
-			LOG(INFO) << "DEU RUIM";
-		}
-	
-	}
-
-	chrono.lap();
-
-	for (auto x = 0; x <= 999999; ++x) {
-		reg.get(typeid(Position).hash_code());
-		if (pools[1].value != 10) {
-			LOG(INFO) << "DEU RUIM";
-		}
-	}
-
-
-	//entityManager.view<Test>().each();
-	
-
-
-
-	chrono.end();
-	std::string asd;
-	std::cin >> asd;
-
 
 
 
